aaf57205dfdca67e46732414e0b319ce52481cc4
Barry Wallis <barry.wallis@outlook.com>
2022-10-25 15:02:51 -0700

    The mock API controller for chapter 1 (found in the appendix).

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # software-architecture-by-example
+
+THis repository contains my version of the examples from the Apress book _Software Architecture by 
+Example_ by Paul Michaels. I have updated the code to C# 11 and .NET 7 taking advantage of newer 
+constrcuts (e.g., records) where possible.
--- booking-api/Controllers/ExternalTicketBookingController.cs
+++ booking-api/Controllers/ExternalTicketBookingController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace booking_api.Controllers;
+/// <summary>
+/// This is a mock class to provide simple sample data.
+/// </summary>
 [ApiController]
 [Route("[controller]/[action]")]
 public class ExternalTicketBookingController : ControllerBase
@@ -11,10 +14,17 @@ public class ExternalTicketBookingController : ControllerBase
     private static readonly DateOnly _eventDate = new(2021, 06, 23);
     private static readonly decimal[] _prices = new[] { 100.50M, 260.65M, 540.10M };
 
+    /// <summary>
+    /// Create a new external ticketing controller.
+    /// </summary>
     public ExternalTicketBookingController()
     {
     }
 
+    /// <summary>
+    /// Get an enumaration of all available tickets,
+    /// </summary>
+    /// <returns>The enumaration of available tickets.</returns>
     [HttpGet]
     public IEnumerable<TicketInformation> GetTickets()
     {
@@ -31,6 +41,11 @@ public class ExternalTicketBookingController : ControllerBase
         return tickets;
     }
 
+    /// <summary>
+    /// Request to reserve a ticket.
+    /// </summary>
+    /// <param name="seatCode">The seat code of the ticket to be reserved.</param>
+    /// <returns>The result of the request.</returns>
     [HttpPost("{seatCode?}")]
 #pragma warning disable IDE0060 // Remove unused parameter
     public async Task<IActionResult> ReserveTicket(string seatCode)
@@ -40,6 +55,11 @@ public class ExternalTicketBookingController : ControllerBase
         return _random.Next(10) == 1 ? BadRequest() : Ok();
     }
 
+    /// <summary>
+    /// Request to purchase a ticket.
+    /// </summary>
+    /// <param name="seatCode">The seat code of the ticket to be purchased.</param>
+    /// <returns>The result of the request.</returns>
     [HttpPost("{seatCode?}")]
 #pragma warning disable IDE0060 // Remove unused parameter
     public async Task<IActionResult> PurchaseTicket(string seatCode)
