59de6572bcbbd43944567cc5a51c45b3c78cbaba
Barry Wallis <barry.wallis@outlook.com>
2022-10-25 14:53:46 -0700

    Add project files.

---
+++ booking-api/Controllers/ExternalTicketBookingController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace booking_api.Controllers;
+[ApiController]
+[Route("[controller]/[action]")]
+public class ExternalTicketBookingController : ControllerBase
+{
+    private static readonly Random _random = new();
+    private static readonly int _capacity = 1_200_000;
+    private static readonly string _eventCode = "GLS_21";
+    private static readonly DateOnly _eventDate = new(2021, 06, 23);
+    private static readonly decimal[] _prices = new[] { 100.50M, 260.65M, 540.10M };
+
+    public ExternalTicketBookingController()
+    {
+    }
+
+    [HttpGet]
+    public IEnumerable<TicketInformation> GetTickets()
+    {
+        List<TicketInformation> tickets = new();
+        for (int i = 0; i < _prices.Length; i++)
+        {
+            tickets.Add(new TicketInformation(_eventCode,
+                                              _eventDate,
+                                              _prices[_random.Next(_prices.Length)],
+                                              SeatCode: "NA",
+                                              Quantity: _capacity / _prices.Length));
+        }
+
+        return tickets;
+    }
+
+    [HttpPost("{seatCode?}")]
+#pragma warning disable IDE0060 // Remove unused parameter
+    public async Task<IActionResult> ReserveTicket(string seatCode)
+#pragma warning restore IDE0060 // Remove unused parameter
+    {
+        await Task.Delay(1_000);
+        return _random.Next(10) == 1 ? BadRequest() : Ok();
+    }
+
+    [HttpPost("{seatCode?}")]
+#pragma warning disable IDE0060 // Remove unused parameter
+    public async Task<IActionResult> PurchaseTicket(string seatCode)
+#pragma warning restore IDE0060 // Remove unused parameter
+    {
+        await Task.Delay(2_000);
+        return _random.Next(10) == 1 ? BadRequest() : Ok();
+    }
+}
---
+++ booking-api/TicketInformation.cs
@@ -0,0 +1,17 @@
+﻿namespace booking_api;
+
+/// <summary>
+/// All the information needed for a ticket.
+/// </summary>
+/// <param name="EventCode">The event code.</param>
+/// <param name="EventDate">The event date.</param>
+/// <param name="Price">The ticket price.</param>
+/// <param name="SeatCode">The seat code.</param>
+/// <param name="Quantity">The number of tickets.</param>
+public record TicketInformation(string EventCode,
+                                DateOnly EventDate,
+                                decimal Price,
+                                string SeatCode,
+                                int Quantity)
+{
+}
